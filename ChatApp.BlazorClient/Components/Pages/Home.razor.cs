d79717de86ffd5859003390cd410971eb88197c3
SPYROS\Spyros <sponaris@gmail.com>
2024-07-30 22:30:13 +0300

    Add project files.

---
+++ ChatApp.BlazorClient/Components/Pages/Home.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ChatApp.BlazorClient.Components.Pages;
+
+public partial class Home : IAsyncDisposable
+{
+    private HubConnection? hubConnection;
+    private List<string> messages = [];
+    private string? username;
+    private string? message;
+    private bool isConnected => hubConnection?.State == HubConnectionState.Connected;
+
+    protected override async Task OnInitializedAsync()
+    {
+        hubConnection = new HubConnectionBuilder()
+        .WithUrl("https://localhost:7159/chatHub")
+        .Build();
+
+        hubConnection.On<string, string>("ReceiveMessage", (username, message) =>
+        {
+            var encodedMessage = $"{username}: {message}";
+            messages.Add(encodedMessage);
+            StateHasChanged();
+        });
+
+        await hubConnection.StartAsync();
+    }
+
+    private async Task SendMessage()
+    {
+        if (hubConnection != null && isConnected)
+        {
+            await hubConnection!.InvokeAsync("SendMessage", username, message);
+            message = string.Empty;
+        }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (hubConnection is not null)
+        {
+            await hubConnection.DisposeAsync();
+        }
+    }
+}
